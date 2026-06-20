8dc26c5fd8bb03f90be8877b971195aea2ac5e7d
Blorptopia <blorptopia@example.com>
2026-06-20 11:16:05 +0200

    init

---
+++ src/elements/game.ts
@@ -0,0 +1,9 @@
+import { html, LitElement, type HTMLTemplateResult } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("curse-game")
+export class GameElement extends LitElement {
+	protected render(): HTMLTemplateResult {
+	   	return html``; 
+	}
+}
