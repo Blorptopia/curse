6ae718912051a58002fe78dbf8d7393088e4f881
Blorptopia <blorptopia@example.com>
2026-06-21 14:50:56 +0200

    show customers

--- src/elements/game.ts
+++ src/elements/game.ts
@@ -1,28 +1,48 @@
 import { html, type HTMLTemplateResult, LitElement, type CSSResultGroup, css } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, state } from "lit/decorators.js";
 import "./customer_portrait";
 import "./hot_plate";
 import "./cup";
 import "./ingredient_icon";
+import "./customer_shadow";
+import { map } from "lit/directives/map.js";
+import type { Order, OrderTemplate } from "../types/order";
+import { MAX_ORDERS_PER_DAY } from "../config";
+import type { CustomerId } from "../types/customer";
+
+const RANDOM_VALUE_VARIATION: number = 0.1;
 
 @customElement("curse-game")
 export class GameElement extends LitElement {
+	@state()
+	private dayIndex: number;
+	@state()
+	private orders: Order[];
+	@state()
+	private deadCustomerIds: CustomerId[];
+
+	public constructor() {
+		super();
+
+		this.dayIndex = 0;
+		this.deadCustomerIds = [];
+		this.orders = this.createOrders();
+	}
+
 	protected render(): HTMLTemplateResult {
 	    return html`
 			<section id="window-frame">
 				<div id="left-window" class="window"></div>
 				<div id="right-window" class="window"></div>
 
-				<div id="customer-container">
-					<curse-customer-portrait class="i-hate-images" customerid="JACK"></curse-customer-portrait>
-				</div>
+				${map(this.orders, (order, index) => this.renderCustomer(order.customerId, index))}
 				<div id="hot-plate-container">
-					<curse-hot-plate class="i-hate-images"></curse-hot-plate>
+					<curse-hot-plate></curse-hot-plate>
 				</div>
 			</section>
 			<section id="shelf">
 				<div id="essentials-row" class="shelf-row">
-					<curse-cup class="i-hate-images"></curse-cup>
+					<curse-cup></curse-cup>
 				</div>
 				<div id="ingredients-row" class="shelf-row">
 					<curse-ingredient-icon ingredientid="BLUE_PILL"></curse-ingredient-icon>
@@ -30,6 +50,135 @@ export class GameElement extends LitElement {
 				</div>
 			</section>
 		`
+	}
+	private renderCustomer(customerId: CustomerId, orderIndex: number): HTMLTemplateResult {
+		if (orderIndex > 4) {
+			return html``;
+		}
+		return html`
+			<div
+				class="customer-container"
+				data-index=${orderIndex}
+			>
+				${orderIndex === 0 ? html`
+					<curse-customer-portrait
+						customerid=${customerId}
+					></curse-customer-portrait>
+				` : html`
+					<curse-customer-shadow
+						customerid=${customerId}
+					></curse-customer-portrait>
+				`}
+			</div>
+		`
+
+	}
+	private createOrderTemplates(): OrderTemplate[] {
+		const JACK_INTERACTIONS: OrderTemplate[] = [
+			{
+				name: "Genius potion",
+				description: "My wife keeps winning at trivia and maths, please make me a genius potion.",
+				customerId: "JACK",
+				targetColor: "#00FF00",
+				baseValue: 1
+			},
+			{
+				name: "Stupid potion",
+				description: "I never lose to my wife anymore, but somehow, i learned to speak dog. All my dog does is argue politics with me now. Please make me a stupid potion.",
+				customerId: "JACK",
+				targetColor: "#00FFFF",
+				baseValue: 1
+			},
+			{
+				name: "Antidepressant potion",
+				description: "Now that i'm stupid, both my dog and wife keep beating me at everything.\nI still speak dog.\nPlease make me an anti-depression potion.",
+				customerId: "JACK",
+				targetColor: "#00FFFF",
+				baseValue: 1
+			},
+
+		];
+		const templates: OrderTemplate[] = [];
+		if (this.dayIndex === 0) {
+			templates.push(JACK_INTERACTIONS[0]);
+		}
+		if (this.dayIndex === 1) {
+			templates.push(JACK_INTERACTIONS[1]);
+		}
+		if (this.dayIndex === 2) {
+			templates.push(JACK_INTERACTIONS[2]);
+		}
+
+		const RANDOM_ORDER_TEMPLATES: OrderTemplate[] = [
+			{
+				name: "Armor potion",
+				customerId: "JACK",
+				targetColor: "#00FFFF",
+				baseValue: 1
+			},
+			{
+				name: "Upside-down potion",
+				customerId: "JACK",
+				targetColor: "#00FFFF",
+				baseValue: 1
+			},
+		];
+		
+		const desiredRandomOrderCount = MAX_ORDERS_PER_DAY - templates.length;
+		for (let i = 0; i < desiredRandomOrderCount; i++) {
+			const template = RANDOM_ORDER_TEMPLATES[Math.floor(Math.random() * RANDOM_ORDER_TEMPLATES.length)]!;
+			templates.push(template);
+		}
+		return templates;
+	}
+	private createOrders(): Order[] {
+		const templates = this.createOrderTemplates();
+		const templatesWithoutDeadCustomers = templates.filter(template => !this.deadCustomerIds.includes(template.customerId));
+
+		const orders = templatesWithoutDeadCustomers.map(template => {
+			const valueRandomMultiplier = 1 + (Math.random() * RANDOM_VALUE_VARIATION) - (RANDOM_VALUE_VARIATION / 2);
+			const valueReputationMultipier = 1; // TODO: Change
+			const valueDayMultiplier = this.getDayValueMultiplier();
+			const value = Math.floor(template.baseValue * valueRandomMultiplier * valueReputationMultipier * valueDayMultiplier);
+			const order = {
+				id: crypto.randomUUID(),
+				name: template.name,
+				description: template.description,
+				customerId: template.customerId,
+				targetColor: template.targetColor,
+				value
+			} satisfies Order;
+			return order;
+		});
+		return orders;
+	}
+	private getDayValueMultiplier(): number {
+		if (this.dayIndex === 0) {
+			return 50;
+		}
+		if (this.dayIndex === 1) {
+			return 100;
+		}
+		if (this.dayIndex === 2) {
+			return 100;
+		}
+		const baseValue = 150;
+		return baseValue - (this.dayIndex * 10);
+	}
+	private getLoanSharkPayment(): number {
+		if (this.dayIndex === 0) {
+			return 50;
+		}
+		if (this.dayIndex === 1) {
+			return 150;
+		}
+		if (this.dayIndex === 2) {
+			return 250;
+		}
+		if (this.dayIndex === 3) {
+			return 350;
+		}
+		return 550;
 	}
 	public static styles?: CSSResultGroup = css`
 		#window-frame {
@@ -73,15 +222,8 @@ export class GameElement extends LitElement {
 			display: flex;
 			gap: 1rem;
 		}
-		.i-hate-images {
-			height: calc(var(--source-height) * var(--size-multiplier));
-			width: calc(var(--source-width) * var(--size-multiplier));
-		}
-		#customer-container {
+		.customer-container {
 			position: absolute;
-			left: 0;
-			bottom: 0;
-			width: 100%;
 			overflow-y: hidden;
 			z-index: -100;
 
@@ -90,8 +232,34 @@ export class GameElement extends LitElement {
 
 			curse-customer-portrait {
 				--size-multiplier: .35rem;
-				anchor-name: --customer;
+				anchor-name: --active-customer;
 			}
+			curse-customer-shadow {
+				--size-multiplier: .25rem;
+			}
+		}
+		.customer-container[data-index="0"] {
+			z-index: -50;
+			width: 100%;
+			left: 0;
+			bottom: 0;
+		}
+		.customer-container[data-index="1"] {
+			left: 0;
+			bottom: -7rem;
+		}
+		.customer-container[data-index="2"] {
+			right: 0;
+			bottom: -5rem;
+			transform: rotateY(180deg);
+		}
+		.customer-container[data-index="3"] {
+			left: 20rem;
+			bottom: -5rem;
+		}
+		.customer-container[data-index="4"] {
+			right: 20rem;
+			bottom: -3rem;
 		}
 		#hot-plate-container {
 			position: absolute;
@@ -123,6 +291,22 @@ export class GameElement extends LitElement {
 			}
 			#space-saving-row {
 				display: flex;
+			}
+		}
+		@media (max-width: 1000px) {
+			.customer-container[data-index="1"] {
+				display: none;
+			}
+			.customer-container[data-index="2"] {
+				display: none;
+			}
+		}
+		@media (max-width: 1900px) {
+			.customer-container[data-index="3"] {
+				display: none;
+			}
+			.customer-container[data-index="4"] {
+				display: none;
 			}
 		}
 	`;
