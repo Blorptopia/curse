562ed44139bf4120cdce0fc7f6543d5c792b2c70
Blorptopia <blorptopia@example.com>
2026-06-21 16:05:35 +0200

    build game over and game pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,40 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
-import { defineConfig } from "vite";
+import { defineConfig, type ConfigEnv } from "vite";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 
-export default defineConfig({
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const ENTRYPOINTS = [
+	"index.html",
+	"game.html",
+	"game-over.html"
+];
+
+
+const config = defineConfig({
+	mode: "mpa",
 	plugins: [cloudflare()],
+});
+
+function createInputFiles(): Record<string, string> {
+	const inputs: Record<string, string> = {};
+	for (const [index, entrypoint] of ENTRYPOINTS.entries()) {
+		inputs[index.toString()] = resolve(__dirname, entrypoint);
+	}
+	return inputs;
+}
+
+export default defineConfig((env: ConfigEnv) => {
+	if (env.mode === "production") {
+		return {
+			...config,
+			build: {
+				rollupOptions: {
+					input: createInputFiles()
+				}
+			}
+		}
+	}
+	return config;
 });
