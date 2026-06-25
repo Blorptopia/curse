d12018460420c8571481c0736119eb789f9ff6ab
MoreRustLessAccess <179813162+MoreRustLessAccess@users.noreply.github.com>
2026-06-25 12:53:58 +0200

    multiple poses per character

--- src/data/customer.ts
+++ src/data/customer.ts
@@ -1,7 +1,6 @@
 import type { CustomerId } from "../types/customer";
 
 export const CUSTOMER_ID_TO_NAME = {
-	LOANS_HARK_PRE_EXPLOSION: "Loans Hark",
 	LOANS_HARK: "Loans Hark",
 	JACK: "Jack",
 	JOANY: "Joany"
--- src/elements/customer_portrait.ts
+++ src/elements/customer_portrait.ts
@@ -2,25 +2,33 @@ import { type HTMLTemplateResult, LitElement, html, type CSSResultGroup, css } f
 import { customElement, property } from "lit/decorators.js";
 import type { CustomerId } from "../types/customer";
 
-import JackURL from "../assets/customers/jack/portrait.png";
-import JoanyURL from "../assets/customers/joany/portrait.png";
+import JackDefaultURL from "../assets/customers/jack/poses/default.png";
+import JoanyDefaultURL from "../assets/customers/joany/poses/default.png";
 
 const ID_TO_IMAGES = {
-	JACK: JackURL,
-	JOANY: JoanyURL
-} satisfies Record<CustomerId, string>;
+	JACK: {
+		default: JackDefaultURL,
+	},
+	JOANY: {
+		default: JoanyDefaultURL
+	}
+} satisfies Record<CustomerId, Record<string, string>>;
 
 @customElement("curse-customer-portrait")
 export class CustomerPortraitElement extends LitElement {
 	@property({type: String})
 	public customerId: CustomerId;
+	@property({type: String})
+	public pose: string;
 
 	public constructor() {
 		super();
 		this.customerId = "GUSTAVO";
+		this.pose = "default";
 	}
 	protected render(): HTMLTemplateResult {
-		const imageUrl = ID_TO_IMAGES[this.customerId];
+		const images = ID_TO_IMAGES[this.customerId];
+		let imageUrl = (images[this.pose] ?? images["default"])!;
 	   	return html`
 			<img src=${imageUrl} alt="">
 		`;
