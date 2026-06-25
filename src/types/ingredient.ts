239b28398e46e889147604348ef532b4afc53f68
platinumaniac <platinumonsteam@gmail.com>
2026-06-25 21:55:58 +0200

    added ingredient drawing

--- src/types/ingredient.ts
+++ src/types/ingredient.ts
@@ -1,3 +1,4 @@
+import type { ColorInstance } from "color";
 import type { INGREDIENTS } from "../data/ingredients";
 export type IngredientId = keyof typeof INGREDIENTS;
 export type IngredientInfo = {
@@ -5,6 +6,7 @@ export type IngredientInfo = {
 	description: string;
 	effects: IngredientEffects;
 	price: number;
+	color: ColorInstance;
 };
 
 type IngredientEffects = {
